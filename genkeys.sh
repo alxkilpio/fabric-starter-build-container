561bff0c3f9142013ea55f6de4d0acd01984a848
skilpio <skilpio@gmail.com>
2022-06-22 14:55:08 +0300

    add ssh keys generate script

---
+++ genkeys.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+BUILDING_CONTAINER_NAME="fabric_starter_builder_container"
+CONTAINER_USER="gradle"
+CONTAINER_SSH_CONF_DIR="/home/${CONTAINER_USER}/.ssh"
+KEYS_DIR='./keys'
+
+mkdir -p ${KEYS_DIR}
+find ${KEYS_DIR} -type f -exec rm {} \;
+
+containerIP=$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' ${BUILDING_CONTAINER_NAME})
+echo "${BUILDING_CONTAINER_NAME} container IP address: ${containerIP}"
+
+ssh-keygen -t rsa -b 4096 -f ${KEYS_DIR}/id_rsa_builder -C "jenkins@henkins" -N "" -q
+
+docker container exec --user ${CONTAINER_USER} -it  fabric_starter_builder_container bash -c \
+    "mkdir -p ${CONTAINER_SSH_CONF_DIR}; chmod 700 ${CONTAINER_SSH_CONF_DIR}"
+
+docker cp ${KEYS_DIR}/id_rsa_builder.pub ${BUILDING_CONTAINER_NAME}:${CONTAINER_SSH_CONF_DIR}/
+
+docker container exec --user ${CONTAINER_USER} -it  ${BUILDING_CONTAINER_NAME} bash -c \
+    "cat ${CONTAINER_SSH_CONF_DIR}/id_rsa_builder.pub >> ${CONTAINER_SSH_CONF_DIR}/authorized_keys; chmod 700 ${CONTAINER_SSH_CONF_DIR}/authorized_keys"
+
+ssh -o StrictHostKeyChecking=no ${CONTAINER_USER}@${containerIP} -i ${KEYS_DIR}/id_rsa_builder hostname
