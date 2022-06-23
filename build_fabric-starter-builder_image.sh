a97d2b618d1567c0cdeca617aa7c9ee3367263d9
skilpio <skilpio@gmail.com>
2022-06-23 16:38:13 +0300

    change node install mechanism

--- build_fabric-starter-builder_image.sh
+++ build_fabric-starter-builder_image.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-#docker image build -t kilpio/jenkins_dockerized .
-repo=${1:-'kilpio'}
+#docker image build -t alxkilpio/jenkins_dockerized .
+repo=${1:-'alxkilpio'}
 docker image build --no-cache -t ${repo}/fabric-starter-builder-container:latest .
 
 
